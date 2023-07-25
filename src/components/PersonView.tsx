49b2940bee6b8ff791772367177871315128fa17
aaa2002 <alexand2002@gmail.com>
2023-07-25 15:41:38 +0300

    Commit #5 - Update Contact working

--- src/components/PersonView.tsx
+++ src/components/PersonView.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import mockData from "../data/mockData";
 import { useNavigate } from "react-router-dom";
@@ -12,7 +12,15 @@ function PersonView() {
   const person = mockData.find((item) => item.id === currentId);
 
   useEffect(() => {
-    const currentPerson = mockData[mockData.findIndex((item) => item.id === currentId)];
+    const person = mockData.find((item) => item.id === currentId);
+    if (person) {
+      setFormData(person);
+    }
+  }, [currentId]);
+
+  useEffect(() => {
+    const currentPerson =
+      mockData[mockData.findIndex((item) => item.id === currentId)];
     document.title = `PhoneBook | ${currentPerson.firstName} ${currentPerson.lastName}`;
   });
 
@@ -24,13 +32,73 @@ function PersonView() {
     navigate("/");
   };
 
+  const [editOn, setEditOn] = useState(false);
+  const [data, setData] = useState(mockData);
+
+  const handleEdit = () => {
+    setEditOn(true);
+  };
+
+  interface PersonData {
+    id: number;
+    firstName: string;
+    lastName: string;
+    company: string;
+    phoneNumber: string;
+    notes: string;
+  }
+
+  const initialFormData: PersonData = {
+    id: 0,
+    firstName: "",
+    lastName: "",
+    company: "",
+    phoneNumber: "",
+    notes: "",
+  };
+  const [formData, setFormData] = useState<PersonData>(initialFormData);
+
+  const handleChange = (event: any) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+    console.log(formData);
+  };
+
+  const handleSave = () => {
+    setEditOn(false);
+
+    const indexToUpdate = mockData.findIndex((item) => item.id === currentId);
+
+    if (indexToUpdate !== -1) {
+      mockData[indexToUpdate] = {
+        ...mockData[indexToUpdate],
+        ...formData,
+      };
+    }
+  };
+
+  useEffect(() => {
+    console.log(formData);
+  }, [formData]);
+
   return (
     <div className="px-8 pt-8 bg-white shadow">
-      {person ? (
+      {!editOn ? (
         <div className="flex flex-col items-center">
-          <h1 className="text-2xl font-bold mb-2">
-            {person ? person.firstName + " " + person.lastName : ""}
-          </h1>
+          <div className="flex flex-row">
+            <button
+              className="text-2xl hover:cursor-pointer hover:text-primary"
+              onClick={handleEdit}
+            >
+              &#9998;&nbsp;&nbsp;
+            </button>
+            <h1 className="text-2xl font-bold mb-2">
+              {person ? person.firstName + " " + person.lastName : ""}
+            </h1>
+          </div>
           <h2 className="text-lg text-gray-600 mb-4">
             {person ? person.company : ""}
           </h2>
@@ -57,7 +125,94 @@ function PersonView() {
           </button>
         </div>
       ) : (
-        <p className="text-gray-500">No person found.</p>
+        <form
+          onSubmit={handleSave}
+          className="max-w-md mx-auto p-6 bg-white rounded-lg shadow-md"
+        >
+          <h1 className="text-2xl font-bold mb-4">Edit</h1>
+          <div className="mb-4">
+            <label
+              htmlFor="firstName"
+              className="block font-medium text-gray-700"
+            >
+              First Name
+            </label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              className="mt-1 p-2 border rounded-md w-full focus:outline-none focus:border-primary"
+              value={formData.firstName}
+              onChange={handleChange}
+            />
+
+            <label
+              htmlFor="lastName"
+              className="block font-medium text-gray-700"
+            >
+              Last Name
+            </label>
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              className="mt-1 p-2 border rounded-md w-full focus:outline-none focus:border-primary"
+              value={formData.lastName}
+              onChange={handleChange}
+            />
+
+            <label
+              htmlFor="lastName"
+              className="block font-medium text-gray-700"
+            >
+              Company
+            </label>
+            <input
+              type="text"
+              id="company"
+              name="company"
+              className="mt-1 p-2 border rounded-md w-full focus:outline-none focus:border-primary"
+              value={formData.company}
+              onChange={handleChange}
+            />
+
+            <label
+              htmlFor="lastName"
+              className="block font-medium text-gray-700"
+            >
+              Phone Number
+            </label>
+            <input
+              type="text"
+              id="phoneNumber"
+              name="phoneNumber"
+              className="mt-1 p-2 border rounded-md w-full focus:outline-none focus:border-primary"
+              value={formData.phoneNumber}
+              onChange={handleChange}
+            />
+
+            <label
+              htmlFor="lastName"
+              className="block font-medium text-gray-700"
+            >
+              Notes
+            </label>
+            <textarea
+              id="notes"
+              name="notes"
+              className="mt-1 p-2 border rounded-md w-full h-32 focus:outline-none focus:border-primary"
+              value={formData.notes}
+              placeholder="Add any notes here..."
+              onChange={handleChange}
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-primary text-white py-2 px-4 rounded-md hover:bg-secondary"
+          >
+            Save
+          </button>
+        </form>
       )}
     </div>
   );
