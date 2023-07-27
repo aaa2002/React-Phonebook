f607eb73b891353dd186601241d49a45ab3f45e0
aaa2002 <alexand2002@gmail.com>
2023-07-27 10:49:02 +0300

    Commit #6 - Filter by Company

--- src/components/PhonebookTable.tsx
+++ src/components/PhonebookTable.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import mockData from "../data/mockData";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
@@ -6,8 +6,32 @@ function PhonebookTable() {
   useEffect(() => {
     document.title = "PhoneBook | People";
   });
+
+  const [companyName, setCompanyName] = useState("");
+
+  const setCompany = (event: any) => {
+    event.preventDefault();
+    setCompanyName(event.target.value);
+    console.log(companyName);
+  };
+
   return (
     <>
+      <div className="flex flex-row h-10 w-full justify-center">
+        <select
+          onChange={setCompany}
+          className="text-center w-full bg-dark text-white hover:bg-secondary hover:cursor-pointer"
+        >
+          <option key={-1} value={""}>
+            Select Company
+          </option>
+          {mockData.map((contact: any) => (
+            <option key={contact.id} value={contact.company}>
+              {contact.company}
+            </option>
+          ))}
+        </select>
+      </div>
       <table className="min-w-full divide-y divide-gray-200 shadow-lg overflow-hidden rounded-lg">
         <thead className="bg-gray-100 text-md">
           <tr>
@@ -26,20 +50,45 @@ function PhonebookTable() {
           </tr>
         </thead>
         <tbody className="bg-white divide-y divide-gray-200">
-          {mockData.map((contact: any) => (
-            <tr key={contact.id}>
-              <td className="px-6 py-4 whitespace-nowrap">
-                <Link to={`/view/${contact.id}`} className="hover:text-dark hover:border-b border-primary">
-                  {contact.firstName} {contact.lastName}
-                </Link>
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap">{contact.company}</td>
-              <td className="px-6 py-4 whitespace-nowrap">
-                {contact.phoneNumber}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap">{contact.notes}</td>
-            </tr>
-          ))}
+          {mockData.map((contact: any) =>
+            companyName === contact.company ? (
+              <tr key={contact.id}>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <Link
+                    to={`/view/${contact.id}`}
+                    className="hover:text-dark hover:border-b border-primary"
+                  >
+                    {contact.firstName} {contact.lastName}
+                  </Link>
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {contact.company}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {contact.phoneNumber}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">{contact.notes}</td>
+              </tr>
+            ) : companyName === "" ? (
+              <tr key={contact.id}>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <Link
+                    to={`/view/${contact.id}`}
+                    className="hover:text-dark hover:border-b border-primary"
+                  >
+                    {contact.firstName} {contact.lastName}
+                  </Link>
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {contact.company}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {contact.phoneNumber}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">{contact.notes}</td>
+              </tr>
+            ) : null
+          )}
         </tbody>
       </table>
     </>
